4afade737a6620a877ec57933c312275ea2b3be1
Márcio Santos do Carmo <x309341@gruposantander.com>
2020-05-02 19:13:23 -0300

    Connection working

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 
+server.setMaxListeners(5);
+server.bind(41234);
+
+const questions = {
+    1: 'Pergunta 1',
+    2: 'pergunta 2',
+    3: 'pergunta 3'
+}
+
 server.on('error', (err) => {
     console.log(`server error:\n${err.stack}`);
     server.close();
@@ -8,12 +17,16 @@ server.on('error', (err) => {
 
 server.on('message', (msg, rinfo) => {
     console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+
+    server.send(JSON.stringify(questions), 0, JSON.stringify(questions).length, rinfo.port, rinfo.address, function (err, bytes) {
+        if (err) 
+            throw err;
+    });
 });
 
-server.on('listening', () => {
-    const address = server.address();
-    console.log(`server listening ${address.address}:${address.port}`);
-});
 
-server.bind(41234);
 // Prints: server listening 0.0.0.0:41234
+server.on('listening', () => {
+    const server = server.address();
+    console.log(`server listening ${server.address}:${server.port}`);
+});
---
+++ src/client-mock.js
@@ -0,0 +1,19 @@
+const dgram = require("dgram");
+const client = dgram.createSocket("udp4");
+
+const data = "New Connection";
+
+client.on('message', (msg) => {
+    console.log(JSON.parse(msg));
+    client.close();
+});
+
+client.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    client.close();
+});
+
+client.send(data, 0, data.length, 41234, "localhost", function (err) {
+    if (err) 
+        throw err;
+});
